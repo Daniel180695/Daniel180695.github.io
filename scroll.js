ca9d99910bb81c8095c37435b596ef053ed0c76a
Daniel180695 <42913363+Daniel180695@users.noreply.github.com>
2019-03-25 12:10:36 +0200

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,36 @@
+const sr = ScrollReveal();
+
+sr.reveal('h1', {
+    origin:'top',
+    distance:'50px',
+    duration:1000,
+    scale:0.5,
+    mobile:true,
+    reset:true
+});
+sr.reveal('p', {
+    origin:'top',
+    distance:'50px',
+    duration:1000,
+    scale:0.5,
+    mobile:true,
+    reset:true
+} );
+sr.reveal('h3', {
+    origin:'top',
+    distance:'50px',
+    duration:1000,
+    scale:0.5,
+    mobile:true,
+    reset:true
+});
+sr.reveal('.skl', {
+    origin:'top',
+    distance:'50px',
+    duration:1000,
+    scale:0.5,
+    mobile:true,
+    reset:true
+});
+
+
